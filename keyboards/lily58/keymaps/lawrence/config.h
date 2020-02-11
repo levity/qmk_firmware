a4894e772e679e7b58416e9679bd37d393159ae7
Lawrence Wang <l@lw.io>
2020-02-10 21:09:59 -0800

    add lawrence keymap

---
+++ keyboards/lily58/keymaps/lawrence/config.h
@@ -0,0 +1,32 @@
+
+#define GRAVE_ESC_ALT_OVERRIDE
+
+#define MOUSEKEY_DELAY 40
+#define MOUSEKEY_INTERVAL 32
+#define MOUSEKEY_MAX_SPEED 6
+#define MOUSEKEY_TIME_TO_MAX 12
+#define MOUSEKEY_WHEEL_MAX_SPEED 1
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 0
+
+#define MK_3_SPEED
+#define MK_MOMENTARY_ACCEL
+
+// cursor refresh rate
+#define MK_C_INTERVAL_UNMOD 16
+#define MK_C_INTERVAL_0 16
+#define MK_C_INTERVAL_1 16
+#define MK_C_INTERVAL_2 16
+
+// cursor speed
+#define MK_C_OFFSET_UNMOD 10
+#define MK_C_OFFSET_0 4
+#define MK_C_OFFSET_1 18
+#define MK_C_OFFSET_2 26
+
+// wheel speed
+#define MK_W_INTERVAL_0 96
+#define MK_W_INTERVAL_1 96
+#define MK_W_INTERVAL_UNMOD 96
+
+// https://beta.docs.qmk.fm/features/feature_advanced_keycodes#tap-hold-configuration-options
+#define RETRO_TAPPING 1
