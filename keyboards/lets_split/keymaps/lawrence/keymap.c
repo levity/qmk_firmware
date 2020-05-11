484855cdafcb202c6a05df1adab5b0045dfffd5f
Lawrence Wang <l@lw.io>
2020-05-11 10:40:49 -0700

    add media layer to Let's Split (LS)
    lily: use the same three-row media layer
    LS: add backspace as Fn-H

--- keyboards/lets_split/keymaps/lawrence/keymap.c
+++ keyboards/lets_split/keymaps/lawrence/keymap.c
@@ -2,15 +2,13 @@
 
 #define _COLMAK  0
 #define _NUMPAD  1
-#define _FN      2
+#define _MEDIA   2
+#define _FN      3
 
 #define MO_FN    MO(_FN)
-#define MO_MEDI  MO(_MEDIA)
 #define MO_NUMP  MO(_NUMPAD)
-#define TG_COLM  TG(_COLMAK)
-#define TG_QWER  TG(_QWERTY)
-#define TG_MOUS  TG(_MOUSE)
-#define TG_NUMP  TG(_NUMPAD)
+#define MO_MEDI  MO(_MEDIA)
+
 
 #define K_BACK   LGUI(KC_LBRC)
 #define K_FWD    LGUI(KC_RBRC)
@@ -46,17 +44,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 KC_ESC , KC_Q   , KC_W   , KC_F   , KC_P   , KC_B   ,       KC_J   , KC_L   , KC_U   , KC_Y   , KC_SCLN, KC_BSPC, \
 KC_LCTL, MT_LSFT, MT_LALT, KC_S   , KC_T   , KC_G   ,       KC_H   , KC_N   , KC_E   , MT_RALT, MT_RSFT, KC_QUOT, \
 KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   ,       KC_K   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, K_SH_BS, \
-KC_TAB , MO_NUMP, _______, MO_FN  , KC_LGUI, KC_SPC ,       KC_ENT , MO_FN  , ___X___, ___X___, ___X___, ___X___),
+___X___, KC_TAB , MO_NUMP, MO_FN  , KC_LGUI, KC_SPC ,       KC_ENT , MO_FN  , KC_RGUI, MO_MEDI, ___X___, ___X___),
 
 [_NUMPAD] = LAYOUT(\
-_______, ___X___, ___X___, ___X___, ___X___, ___X___,       KC_PSLS, KC_P7  , KC_P8  , KC_P9  , ___X___, ___X___, \
-_______, _______, _______, _______, _______, _______,       KC_PAST, KC_P4  , KC_P5  , KC_P6  , ___X___, ___X___, \
-_______, _______, _______, _______, _______, _______,       KC_PMNS, KC_P1  , KC_P2  , KC_P3  , ___X___, ___X___, \
+_______, _______, _______, _______, _______, _______,       KC_PSLS, KC_P7  , KC_P8  , KC_P9  , _______, _______, \
+_______, _______, _______, _______, _______, _______,       KC_PAST, KC_P4  , KC_P5  , KC_P6  , _______, _______, \
+_______, _______, _______, _______, _______, _______,       KC_PMNS, KC_P1  , KC_P2  , KC_P3  , _______, _______, \
 _______, _______, _______, _______, _______, _______,       KC_PPLS, KC_PEQL, KC_P0  , KC_PDOT, _______, _______),
 
+[_MEDIA] = LAYOUT(\
+K_SLEEP, ___X___, ___X___, ___X___, ___X___, ___X___,       ___X___, ___X___, ___X___, KC_MPRV, KC_MPLY, KC_MNXT, \
+___X___, ___X___, ___X___, ___X___, ___X___, ___X___,       ___X___, ___X___, ___X___, KC_MUTE, KC_VOLD, KC_VOLU, \
+___X___, ___X___, ___X___, ___X___, ___X___, ___X___,       ___X___, ___X___, K_LSCR , K_RSCR , KC_BRID, KC_BRIU, \
+___X___, ___X___, ___X___, ___X___, ___X___, ___X___,       ___X___, ___X___, ___X___, _______, _______, _______),
+
+
 [_FN] = LAYOUT(\
-KC_GRV , _______, KC_LCBR, KC_RCBR, KC_GRV , KC_TILD,       _______, K_BACK , KC_UP  , K_FWD  , _______, _______, \
-_______, _______, KC_LPRN, KC_RPRN, KC_EQL , KC_PLUS,       _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, \
-_______, _______, KC_LBRC, KC_RBRC, KC_MINS, KC_UNDS,       _______, _______, K_LTAB , K_RTAB , KC_BSLS, _______, \
+_______, _______, KC_LCBR, KC_RCBR, KC_GRV , KC_TILD,       _______, K_BACK , KC_UP  , K_FWD  , _______, _______, \
+_______, _______, KC_LPRN, KC_RPRN, KC_EQL , KC_PLUS,       KC_BSPC, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, \
+_______, _______, KC_LBRC, KC_RBRC, KC_MINS, KC_UNDS,       _______, _______, K_LTAB , K_RTAB , _______, _______, \
 RESET  , _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______),
 };
