dcea86fa0b86acf8889d0f4b8e08a446ffce36a2
Lawrence Wang <l@lw.io>
2020-07-02 21:17:56 -0700

    number-row-less layout
    make new Sym layer for shifted num keys

--- keyboards/lily58/keymaps/lawrence/keymap.c
+++ keyboards/lily58/keymaps/lawrence/keymap.c
@@ -2,9 +2,10 @@
 
 #define _COLMAK  0
 #define _NUMPAD  2
-#define _MOUSE   3
-#define _MEDIA   4
-#define _FN      5
+#define _SYM     3
+#define _MOUSE   4
+#define _MEDIA   5
+#define _FN      6
 
 #define MO_FN    MO(_FN)
 #define MO_MEDI  MO(_MEDIA)
@@ -24,33 +25,44 @@
 #define K_RAPP   LGUI(KC_TAB)
 #define K_LSCR   LCTL(LSFT(KC_LEFT))
 #define K_RSCR   LCTL(LSFT(KC_RGHT))
+#define K_DIVVY  LCTL(LGUI(KC_TAB))
 
-#define MT_LSFT  LSFT_T(KC_A)
-#define MT_RSFT  RSFT_T(KC_O)
+#define MSFT_A  LSFT_T(KC_A)
+#define MSFT_S  LSFT_T(KC_S)
+#define MSFT_T  LSFT_T(KC_T)
+#define MSFT_N  RSFT_T(KC_N)
+#define MSFT_E  RSFT_T(KC_E)
+#define MSFT_O  RSFT_T(KC_O)
 
-#define MT_LALT  LALT_T(KC_R)
-#define MT_RALT  RALT_T(KC_I)
+#define MALT_R  LALT_T(KC_R)
+#define MALT_I  RALT_T(KC_I)
 
-#define MT_LGUI  LGUI_T(KC_T)
-#define MT_RGUI  RGUI_T(KC_N)
+#define LSYM_QT LT(_SYM, KC_QUOT)
 
 #define ___X___  KC_NO
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Colemak with left-hand mod-DH
 [_COLMAK] = LAYOUT(\
-KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                   KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS , \
+KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                   KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, \
 KC_TAB , KC_Q   , KC_W   , KC_F   , KC_P   , KC_B   ,                   KC_J   , KC_L   , KC_U   , KC_Y   , KC_SCLN, KC_BSLS, \
-KC_LCTL, MT_LSFT, MT_LALT, KC_S   , KC_T   , KC_G   ,                   KC_H   , KC_N   , KC_E   , MT_RALT, MT_RSFT, KC_QUOT, \
+KC_LCTL, MSFT_A , MALT_R , KC_S   , MSFT_T , KC_G   ,                   KC_H   , MSFT_N , KC_E   , MALT_I , MSFT_O , LSYM_QT, \
 KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   , KC_ESC , KC_BSPC, KC_K   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, \
                            MO_MOUS, MO_NUMP, KC_LGUI, KC_SPC , KC_ENT , MO_FN  , KC_RGUI, MO_MEDI),
 
 [_NUMPAD] = LAYOUT(\
 ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,                   ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, \
-_______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , _______,                   _______, KC_7   , KC_8   , KC_P9  , ___X___, ___X___, \
-_______, KC_F5  , KC_F6  , KC_F7  , KC_F8  , _______,                   _______, KC_4   , KC_5   , KC_6   , KC_P0  , ___X___, \
-_______, KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______, TG_NUMP, _______, _______, KC_1   , KC_2   , KC_3   , ___X___, ___X___, \
+K_DIVVY, _______, _______, _______, _______, _______,                   KC_PAST, KC_7   , KC_8   , KC_P9  , ___X___, ___X___, \
+KC_F11 , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                   KC_PMNS, KC_4   , KC_5   , KC_6   , KC_P0  , ___X___, \
+KC_F12 , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , _______, _______, KC_PPLS, KC_1   , KC_2   , KC_3   , _______, ___X___, \
                            _______, _______, _______, _______, _______, _______, _______, _______),
+
+[_SYM] = LAYOUT(\
+___X___, ___X___, ___X___, ___X___, ___X___, ___X___,                   ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, \
+___X___, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, ___X___, ___X___, ___X___, \
+___X___, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                   ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, \
+___X___, KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, \
+                           ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___),
 
 [_MOUSE] = LAYOUT(\
 ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,                   ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, \
@@ -68,9 +80,9 @@ ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, K_SLEEP, ___X___, ___X___,
 
 [_FN] = LAYOUT(\
 ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,                   ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, \
-_______, _______, KC_LCBR, KC_RCBR, KC_UNDS, KC_PLUS,                   _______, K_BACK , KC_UP  , K_FWD  , _______, _______, \
-_______, _______, KC_LPRN, KC_RPRN, KC_MINS, KC_EQL ,                   KC_WH_D, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, \
-_______, _______, KC_LBRC, KC_RBRC, _______, _______, _______, _______, KC_WH_U, _______, K_LTAB , K_RTAB , K_LSCR , K_RSCR , \
+_______, KC_GRV , KC_LCBR, KC_RCBR, KC_UNDS, KC_PLUS,                   _______, K_BACK , KC_UP  , K_FWD  , _______, _______, \
+_______, KC_BSLS, KC_LPRN, KC_RPRN, KC_MINS, KC_EQL ,                   KC_WH_D, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, \
+_______, _______, KC_LBRC, KC_RBRC, KC_GRV , KC_TILD, _______, _______, KC_WH_U, _______, K_LTAB , K_RTAB , K_LSCR , K_RSCR , \
                            _______, _______, _______, _______, _______, _______, _______, _______),
 };
 
